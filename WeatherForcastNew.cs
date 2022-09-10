5190b168e82db24a52d645d6ab9f21eabac7b366
Hitesh Sakhare <Hitesh.Sakhare@mindtree.com>
2022-09-10 09:08:17 +0530

    made changes in weatherforcast file

--- WeatherForcastNew.cs
+++ WeatherForcastNew.cs
@@ -11,5 +11,6 @@ namespace MedicalStoreManagementSystem
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+        public string day { get; set; }
     }
 }
